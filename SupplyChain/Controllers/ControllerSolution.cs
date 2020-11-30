6e07ec34c8aa543b6d95ac5de379c843a8d4e65a
Gonzalo Brizuela <gonzalobrizuelaortiz@icloud.com>
2020-11-30 14:27:18 -0300

    contenido

---
+++ SupplyChain/Controllers/ControllerSolution.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace SupplyChain
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SolutionController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public SolutionController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IEnumerable<Solution> Get(string CAMPO)
+        {
+            try
+            {
+                string xSQL = string.Format("select CAMPO, VALORC from Solution where CAMPO = 'RUTAOF' OR CAMPO = 'RUTACNC'");
+                return _context.Solution.FromSqlRaw(xSQL).ToList<Solution>();
+            }
+            catch
+            {
+                return new List<Solution>();
+            }
+        }
+    }
+}
---
+++ SupplyChain/Pages/EstadoPage.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SupplyChain;
+using Syncfusion.Blazor.Grids;
+using Syncfusion.Blazor.Navigations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace SupplyChain.Pages.Modelos
+{
+    public class ModeloPageBase : ComponentBase
+    {
+        [Inject] protected CustomHttpClient Http { get; set; }
+        [Inject] protected IJSRuntime JsRuntime { get; set; }
+        protected SfGrid<Solution> Grid;
+        public bool Enabled = true;
+        public bool Disabled = false;
+
+        protected List<Solution> Pedclis = new List<Solution>();
+        protected override async Task OnInitializedAsync()
+        {
+            
+            Pedclis = await Http.GetFromJsonAsync<List<Solution>>("api/Solution");
+
+            await base.OnInitializedAsync();
+        }
+    }
+}
